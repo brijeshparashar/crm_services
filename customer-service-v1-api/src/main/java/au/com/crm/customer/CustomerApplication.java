d71eeb2c8d0b45dab585c65f6b0b16597026366e
brijeshparashar <42034327+brijeshparashar@users.noreply.github.com>
2018-08-03 21:28:03 +1000

    added annotations

--- customer-service-v1-api/src/main/java/au/com/crm/customer/CustomerApplication.java
+++ customer-service-v1-api/src/main/java/au/com/crm/customer/CustomerApplication.java
@@ -3,9 +3,13 @@ package au.com.crm.customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableJpaRepositories
 public class CustomerApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerApplication.class);
