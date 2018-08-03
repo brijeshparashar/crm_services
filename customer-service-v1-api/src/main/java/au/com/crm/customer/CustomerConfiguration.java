b169558bb3ee06c6876ae9099ccb2448bfccc14e
brijeshparashar <42034327+brijeshparashar@users.noreply.github.com>
2018-08-03 19:20:23 +1000

    change

--- customer-service-v1-api/src/main/java/au/com/crm/customer/CustomerConfiguration.java
+++ customer-service-v1-api/src/main/java/au/com/crm/customer/CustomerConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableAspectJAutoProxy
 @EnableJpaRepositories
-@EnableTransactionManagement
 public class CustomerConfiguration {
 
 	
